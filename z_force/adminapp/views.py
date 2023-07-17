8de099688a220a1e8a8d2aa3d75654455a91286d
Roman <rombos1978@gmail.com>
2023-07-17 20:26:53 +0300

    17.07.2023 start

--- z_force/adminapp/views.py
+++ z_force/adminapp/views.py
@@ -1,13 +1,4 @@
 # from django.shortcuts import render
 # from django.http import HttpResponse
-#
-#
-# def hello_Z(request):
-#     return HttpResponse("Hello, Z-Force!")
-#
-#
-# def check_kwargs(request, **kwargs):
-#     return HttpResponse(f"kwargs:<br>{kwargs}")
-
 
 
