d0888fca85ef6ae1e124b5774381785bd4923f77
Roman <rombos1978@gmail.com>
2023-07-25 22:25:17 +0300

    25.07.2023

---
+++ run.py
@@ -0,0 +1,23 @@
+# Для запуска проекта на сервере командой python run.py3
+
+import subprocess
+import threading
+
+
+def run_django():
+    subprocess.call(['python', 'manage.py', 'runserver'])
+
+
+def run_tg_bot():
+    subprocess.call(['python', 'bot.py'])
+
+
+if __name__ == '__main__':
+    django_thread = threading.Thread(target=run_django)
+    telegram_thread = threading.Thread(target=run_tg_bot)
+
+    django_thread.start()
+    telegram_thread.start()
+
+    django_thread.join()
+    telegram_thread.join()
