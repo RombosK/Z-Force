74f3bc87beac18b1c944576633fe6696c88186d4
Nataly Suvorova <suvorova.nata.a@gmail.com>
2024-04-02 18:19:20 +0300

    1

--- static/js/index.js
+++ static/js/index.js
@@ -13,146 +13,175 @@ const arrowLeftElem = document.querySelector(".index__slider_arrowLeft");
 // правая стрелка
 const arrowRightElem = document.querySelector(".index__slider_arrowRight");
 
-let slider = [];
+// let slider = [];
 
-const widthWindow = window.innerWidth;
-// const widthWindow = document.querySelector(".index__slider").clientWidth;
-console.log(widthWindow);
-let flag = true;
+// const widthWindow = window.innerWidth;
+// let flag = true;
 
 // смещаем конвейр на 100vw
-sliderLineElem.style.left = "-" + 100 + "vw";
+// sliderLineElem.style.left = "-" + 100 + "vw";
 
 // перезаписываем массив фото в массив
-for (let i = 0; i < sliderItemsElem.length; i++) {
-  slider[i] = sliderItemsElem[i];
-  sliderItemsElem[i].remove();
-}
+// for (let i = 0; i < sliderItemsElem.length; i++) {
+//   slider[i] = sliderItemsElem[i];
+//   sliderItemsElem[i].remove();
+// }
 
-document.querySelector(".index__slider_items").innerHTML = "";
-
-// console.log(slider);
+// document.querySelector(".index__slider_items").innerHTML = "";
 
 let activeImage = 0;
 
-const initSlider = () => {
-  let div = document.createElement("div");
-  div = slider[activeImage];
-  // console.log(slider[activeImage]);
-  // div.classList.add("index__slider_item");
-  document.querySelector(".index__slider_items").append(div);
-
-  nextImageGenerate();
-  prevImageGenerate();
-};
-
-const nextImageGenerate = () => {
-  let nextImg = activeImage + 1;
-  if (nextImg >= slider.length) {
-    nextImg = 0;
+function slider() {
+  for (let i = 0; i < sliderItemsElem.length; i++) {
+    sliderItemsElem[i].classList.add("index__slider_item-opasity");
   }
-
-  let div = document.createElement("div");
-  div = slider[nextImg];
-  // div.classList.add("index__slider_item");
-  document.querySelector(".index__slider_items").append(div);
-};
-
-const prevImageGenerate = () => {
-  let prevImg = activeImage - 1;
-  if (prevImg < 0) {
-    prevImg = slider.length - 1;
-  }
-
-  let div = document.createElement("div");
-  // if (w) div.style.width = 0;
-
-  div = slider[prevImg];
-
-  // div.classList.add("index__slider_item");
-  document.querySelector(".index__slider_items").prepend(div);
-};
-
-initSlider();
-console.log(document.querySelector(".index__slider_items"));
-console.log(document.querySelectorAll(".index__slider_item"));
-
-const nextSlide = () => {
-  // if (!flag) return;
-  // flag = !flag;
-
-  activeImage++;
-  if (activeImage >= slider.length) {
-    activeImage = 0;
-  }
-  document.querySelector(".index__slider_item").remove();
-  // console.log(document.querySelector(".index__slider_items"));
-
-  nextImageGenerate();
-
-  // prevImageGenerate();
-  console.log(document.querySelector(".index__slider_items"));
-  console.log(document.querySelectorAll(".index__slider_item"));
-
-  console.log(document.querySelector(".index__slider_item"));
-  // animate({
-  //   duration: 1000,
-  //   draw: function (progress) {
-  //     let asd = document.querySelectorAll(".index__slider_item");
-  //     asd[0].style.width = widthWindow * (1 - progress) + "px";
-  //     // console.log(document.querySelector(".index__slider_items"));
-  //   },
-  //   removeElement: document.querySelector(".index__slider_item"),
-  // });
-  console.log(document.querySelector(".index__slider_items"));
-  console.log(document.querySelectorAll(".index__slider_item"));
-
-
-};
-
-const prevSlide = () => {
-  // if (!flag) return;
-  // flag = !flag;
-
-  activeImage--;
-  if (activeImage < 0) activeImage = slider.length - 1;
-
-  prevImageGenerate(true);
-
-  document.querySelector(".index__slider_item:last-child").remove();
-  prevImageGenerate(true);
-
-  // animate({
-  //   duration: 1000,
-  //   draw: function (progress) {
-  //     document.querySelector(".index__slider_item").style.width =
-  //       widthWindow * progress + "px";
-  //   },
-  //   timing: function (step) {
-  //     return step;
-  //   },
-  //   removeElement: document.querySelector(".index__slider_items:last-child"),
-  // });
-};
-
-arrowRightElem.addEventListener("click", nextSlide);
-arrowLeftElem.addEventListener("click", prevSlide);
-
-function animate({ duration, draw, removeElement }) {
-  let start = performance.now();
-
-  requestAnimationFrame(function animate(time) {
-    let step = (time - start) / duration;
-    if (step > 1) step = 1;
-    draw(step);
-    if (step < 1) {
-      requestAnimationFrame(animate);
-    } else {
-      removeElement.remove();
-    }
-  });
+  sliderItemsElem[activeImage].classList.remove("index__slider_item-opasity");
 }
 
+slider();
+
+arrowRightElem.addEventListener("click", () => {
+  if (activeImage >= sliderItemsElem.length - 1) {
+    activeImage = 0;
+  } else {
+    activeImage++;
+  }
+  slider();
+});
+arrowLeftElem.addEventListener("click", () => {
+  if (activeImage <= 0) {
+    activeImage = sliderItemsElem.length - 1;
+  } else {
+    activeImage--;
+  }
+  slider();
+});
+
+setInterval(() => {
+  if (activeImage >= sliderItemsElem.length - 1) {
+    activeImage = 0;
+  } else {
+    activeImage++;
+  }
+  slider();
+}, 3000);
+
+// второй вариант
+// const initSlider = () => {
+//   let div = document.createElement("div");
+//   div = slider[activeImage];
+//   document.querySelector(".index__slider_items").append(div);
+
+//   nextImageGenerate();
+//   prevImageGenerate();
+// };
+
+// const nextImageGenerate = () => {
+//   let nextImg = activeImage + 1;
+//   if (nextImg >= slider.length) {
+//     nextImg = 0;
+//   }
+
+//   let div = document.createElement("div");
+//   div = slider[nextImg];
+//   document.querySelector(".index__slider_items").append(div);
+// };
+
+// const prevImageGenerate = () => {
+//   let prevImg = activeImage - 1;
+//   if (prevImg < 0) {
+//     prevImg = slider.length - 1;
+//   }
+
+//   let div = document.createElement("div");
+//   // if (w) div.style.width = 0;
+
+//   div = slider[prevImg];
+
+//   // div.classList.add("index__slider_item");
+//   document.querySelector(".index__slider_items").prepend(div);
+// };
+
+// initSlider();
+// console.log(document.querySelector(".index__slider_items"));
+// console.log(document.querySelectorAll(".index__slider_item"));
+
+// const nextSlide = () => {
+//   // if (!flag) return;
+//   // flag = !flag;
+
+//   activeImage++;
+//   if (activeImage >= slider.length) {
+//     activeImage = 0;
+//   }
+//   console.log(activeImage);
+
+//   // document.querySelector(".index__slider_item").remove();
+
+//   nextImageGenerate();
+
+//   let asd = document.querySelectorAll(".index__slider_item");
+//   // console.log(asd);
+
+//   animate({
+//     duration: 1000,
+//     draw: function (progress) {
+//       asd[0].style.width = widthWindow * (1 - progress) + "px";
+//     },
+//     removeElement: asd[0],
+//   });
+//   // console.log(asd);
+
+// };
+
+// const prevSlide = () => {
+//   // if (!flag) return;
+//   // flag = !flag;
+
+//   activeImage--;
+//   if (activeImage < 0) activeImage = slider.length - 1;
+
+//   prevImageGenerate(true);
+
+//   document.querySelector(".index__slider_item:last-child").remove();
+//   prevImageGenerate(true);
+
+//   // animate({
+//   //   duration: 1000,
+//   //   draw: function (progress) {
+//   //     document.querySelector(".index__slider_item").style.width =
+//   //       widthWindow * progress + "px";
+//   //   },
+//   //   timing: function (step) {
+//   //     return step;
+//   //   },
+//   //   removeElement: document.querySelector(".index__slider_items:last-child"),
+//   // });
+// };
+
+// arrowRightElem.addEventListener("click", nextSlide);
+// arrowLeftElem.addEventListener("click", prevSlide);
+
+// function animate({ duration, draw, removeElement }) {
+
+//   let start = performance.now();
+
+//   requestAnimationFrame(function animate(time) {
+//     let step = (time - start) / duration;
+
+//     if (step > 1) step = 1;
+
+//     draw(step);
+
+//     if (step < 1) {
+//       requestAnimationFrame(animate);
+//     } else {
+//       removeElement.remove();
+//     }
+//   });
+// }
+
+// третий вариант
 // отвечает за смещение изображения
 // let offset = 0;
 
